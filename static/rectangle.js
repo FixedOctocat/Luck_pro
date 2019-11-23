cd907af581d11df6343fd13d4d6eb0b82c0d243b
dprozz <dmitri.proskuriakov@gmail.com>
2019-11-23 20:36:27 +0300

    fillsquare function

--- static/rectangle.js
+++ static/rectangle.js
@@ -4,13 +4,20 @@ function init () {
     var cofflat = 100, startlat = 3737, stoplat = 3783;
     var myMap = new ymaps.Map('map',{center: [55.753960, 37.620393],zoom: 12,controls:['zoomControl']});
     for(var counterlon = startlon; counterlon <= stoplon; counterlon++){
-        part = []
+        part = [];
         for(var counterlat = startlat; counterlat <= stoplat; counterlat++){
             newSquare = new ymaps.Rectangle([[counterlon / cofflon,counterlat / cofflat],[(counterlon + 1) / cofflon, (counterlat + 1) / cofflat]],{},{fillColor: '#7df9ff33',fillOpacity: 0.5});
             myMap.geoObjects.add(newSquare);
         }
-        districs.push[part]
+        districs.push[part];
     }
 };
+
+function fillSquare(ids){
+    var counter = 0;
+    for(;counter < ids.lenght(); counter++){
+        
+    }
+}
 
 ymaps.ready(init);
