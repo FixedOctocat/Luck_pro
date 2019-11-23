b385cd0153e020021c9bdb701bdc21a788713814
dprozz <dmitri.proskuriakov@gmail.com>
2019-11-23 20:53:58 +0300

    Restore text analyzer by kolayne

---
+++ text_analyzer.py
@@ -0,0 +1,24 @@
+def tonal_analize(string):
+	from dostoevsky.tokenization import RegexTokenizer
+	from dostoevsky.models import FastTextSocialNetworkModel
+
+	tokenizer = RegexTokenizer()
+	model = FastTextSocialNetworkModel(tokenizer=tokenizer)
+	messages = [string]
+	results = model.predict(messages, k=2)
+
+	for message, sentiment in zip(messages, results):
+	    negative = 0
+	    positive = 0
+	    
+	    if 'positive' in sentiment:
+	    	positive += sentiment['positive']
+	    if 'negative' in sentiment:
+	    	negative += sentiment['negative']
+	    
+	    delta = positive - negative
+	    
+	    if delta > 0:
+	    	return delta
+	    else:
+	    	return delta
