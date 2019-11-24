484806414086fc17d67d97791319adce489d5559
dprozz <dmitri.proskuriakov@gmail.com>
2019-11-24 13:48:17 +0300

    get centers work

--- getsquarescenters.py
+++ getsquarescenters.py
@@ -1,9 +1,7 @@
 def square_centers():
     cofflat, cofflon = 100, 200
-    squares = []
+    line = []
     for counterlon in range(11114,11180):
-        line = []
         for counterlat in range(3737,3783):
             line.append([[counterlon / cofflon,counterlat / cofflat],[(counterlon + 1) / cofflon, (counterlat + 1) / cofflat]])
-        squares.append(line)
-    return squares
+    return line
