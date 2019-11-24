71d3df3324598e657d4eef6c732115c42e266933
dprozz <dmitri.proskuriakov@gmail.com>
2019-11-24 14:20:54 +0300

    maybe best squares ever

--- static/rectangle.js
+++ static/rectangle.js
@@ -5,21 +5,22 @@ function div(val, by){
 function init(egor_square){
     return function(){
         myMap = new ymaps.Map('map',{center: [55.753960, 37.620393],zoom: 12.5,controls:['zoomControl']});
+        myMap.geoObjects.add(new ymaps.Rectangle([[1,1],[2,2]],{},{}))
+        myColl = new ymaps.GeoObjectCollection();
         districs = [], part = [];
         for(var counterlon = 0; counterlon < egor_square.length; counterlon++){
             part = [];
             for(var counterlat = 0; counterlat < egor_square[counterlon].length; counterlat++){
                 if (egor_square[counterlon][counterlat][2] >= 0){
                     red = 0;
-                    green = Math.ceil(255 * egor_square[counterlon][counterlat][2]);
+                    green = 100 + Math.ceil(155 * egor_square[counterlon][counterlat][2]);
                 } else {
                     green = 0;
-                    red = Math.ceil(255 * -egor_square[counterlon][counterlat][2]);
+                    red = 100 + Math.ceil(155 * -egor_square[counterlon][counterlat][2]);
                 };
                 color = 'rgb(' + red.toString() + ',' + green.toString() + ',0)';
                 coords = egor_square[counterlon][counterlat]
                 newSquare = new ymaps.Rectangle(coords.slice(0,2),{},{fillColor: color,fillOpacity: 0.5})
-                console.log(newSquare)
                 part.push(newSquare);
             }
             districs.push(part);
@@ -27,14 +28,21 @@ function init(egor_square){
     }
 };
 
+// godef = [[[[55.57, 37.37], [55.575, 37.38], 0.7777253209378436], [[55.57, 37.38], [55.575, 37.39], 0.5891686084953953]]];
+
 function fillSquare(ids){
     return function(){
+        myColl.removeAll();
+        myMap.geoObjects.remove(0);
         for(var counter = 0;counter < ids.length; counter++){
             var gor = 46, gorid = ids[counter] % gor, verid = div(ids[counter],gor);
-            console.log(verid,gorid,districs[verid][gorid])
-            myMap.geoObjects.add(districs[verid][gorid]);
+            myColl.add(districs[verid][gorid]);
         }
-        console.log(1111111111111)
+        myMap.geoObjects.add(myColl);
+        myMap.setBounds(myColl.getBounds());
     }
-    
 }
+
+
+// ymaps.ready(init(godef));
+// ymaps.ready(fillSquare([0,1])); 
